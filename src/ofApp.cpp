9aef2a021b4275780fe8058ebf4ecf9866793a9c
lshap <lauren.shapiro1@gmail.com>
2017-08-17 14:40:30 -0400

    Adding relaxDivergences

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,5 +1,10 @@
 #include "ofApp.h"
 
+static bool abs_compare(int a, int b)
+{
+    return (std::abs(a) < std::abs(b));
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     initPaper(h);
@@ -38,26 +43,22 @@ void ofApp::simulateCapillaryFlow() {
     
 }
 
-float ofApp::getMax(float items[][100]) {
+float ofApp::getMax(float items[][101]) {
     float max = std::numeric_limits<float>::min();
     for (int i=0; i < PAPER_WIDTH; i++ ) {
-        max = fmax(max, *std::max_element(items[i], items[i] + 100));
+        max = fmax(max, *std::max_element(items[i], items[i] + 101, abs_compare));
     }
     return max;
 }
 
 void ofApp::enforceBoundaryConditions() {
-    for (int i = 0; i < PAPER_WIDTH; i++) {
-        for (int j = 0; j < PAPER_HEIGHT; j++) {
+    for (int i = 0; i < PAPER_WIDTH - 1; i++) {
+        for (int j = 0; j < PAPER_HEIGHT - 1; j++) {
             if (M[i][j]  == 0) {
                 u[i][j] = 0;
-                if ( i < PAPER_WIDTH - 1) {
-                    u[i + 1][j] = 0;
-                }
+                u[i + 1][j] = 0;
                 v[i][j] = 0;
-                if ( j < PAPER_HEIGHT - 1) {
-                    v[i][j+ 1] = 0;
-                }
+                v[i][j+ 1] = 0;
             }
         }
     }
@@ -90,8 +91,8 @@ void ofApp::updateVelocities() {
     float u_temp[PAPER_WIDTH][PAPER_HEIGHT];
     float v_temp[PAPER_WIDTH][PAPER_HEIGHT];
     while (t < 1) {
-        for (int i = 0; i < PAPER_WIDTH - 1; i++) {
-            for (int j = 0; j < PAPER_HEIGHT - 1; j++) {
+        for (int i = 0; i < PAPER_WIDTH; i++) {
+            for (int j = 0; j < PAPER_HEIGHT; j++) {
                 float u_i_j = (u[i][j] + u[i + 1][j])/2;
                 float u_i_1_j = (i < PAPER_WIDTH - 2) ? (u[i+1][j] + u[i+2][j])/2 : 0;
                 float A = u_i_j * u_i_j - u_i_1_j * u_i_1_j + u[i+1][j]*v[i][j] - u[i+1][j]*v[i][j+1];
@@ -124,7 +125,32 @@ void ofApp::updateVelocities() {
 }
 
 void ofApp::relaxDivergence() {
-    
+    int MAX_ITER = 50;
+    float tolerance = 0.01;
+    float xi = 0.1;
+    int t = 0;
+    float d_max = 0;
+    do {
+        float u_temp[PAPER_WIDTH][PAPER_HEIGHT];
+        float v_temp[PAPER_WIDTH][PAPER_HEIGHT];
+        memcpy(u_temp, u, sizeof(float)*PAPER_WIDTH*PAPER_HEIGHT);
+        memcpy(v_temp, v, sizeof(float)*PAPER_WIDTH*PAPER_HEIGHT);
+        d_max = 0;
+        for (int i = 0; i < PAPER_WIDTH; i++) {
+            for (int j = 0; j < PAPER_HEIGHT; j++) {
+                float d = xi * (u[i+1][j] - u[i][j] + v[i][j+1] - v[i][j]);
+                p[i][j] += d;
+                u_temp[i+1][j] += d;
+                u_temp[i][j] -= d;
+                v_temp[i+1][j] += d;
+                v_temp[i][j] -= d;
+                d_max = fmax(abs(d), d_max);
+            }
+        }
+        memcpy(u, u_temp, sizeof(float)*PAPER_WIDTH*PAPER_HEIGHT);
+        memcpy(v, v_temp, sizeof(float)*PAPER_WIDTH*PAPER_HEIGHT);
+        t += 1;
+    } while (d_max > tolerance && t < MAX_ITER);
 }
 
 void ofApp::flowOutward() {
--- src/ofApp.h
+++ src/ofApp.h
@@ -30,7 +30,7 @@ class ofApp : public ofBaseApp{
         void relaxDivergence();
         void flowOutward();
         void enforceBoundaryConditions();
-        float getMax(float items[][100]);
+        float getMax(float items[][101]);
     
         const int PAPER_WIDTH = 100;
         const int PAPER_HEIGHT = 100;
@@ -42,8 +42,8 @@ class ofApp : public ofBaseApp{
         float h[100][100];
     
         // staggered grid--size = (m-1)*(n-1)*4 - ((m-2)*(n-1) + (n-2)*(m-1)) + 2*m + 2*n
-        float u[100][100]; // x velocities
-        float v[100][100]; // y velocities
+        float u[101][101]; // x velocities
+        float v[101][101]; // y velocities
     
         float p[100][100]; // water pressure
         float g[10][100][100]; // shallow pigment 
